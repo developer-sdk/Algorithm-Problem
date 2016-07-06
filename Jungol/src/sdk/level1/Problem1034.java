2017b3b673c4473dc0f88511a557b9a9be7a7bf2
118k <fluorite118@gmail.com>
2016-07-06 22:25:26 +0900

    init

---
+++ Jungol/src/sdk/level1/Problem1034.java
@@ -0,0 +1,96 @@
+package sdk.level1;
+
+import java.util.Scanner;
+
+public class Problem1034 {
+
+	public static void main(String[] args) {
+		
+//		Scanner in = new Scanner(System.in);
+//		String str = in.next();
+//		in.close();
+		
+		String str = "ABCDE";
+		char[] chars = str.toCharArray();
+		int[] ints = new int[chars.length];
+		
+		for(int i= 0; i < chars.length; i++) {
+			ints[i] = (int) chars[i] - (int)'A';
+		}
+		
+//		for(int i : ints) {
+//			System.out.printf("%d ", i);
+//		}
+//		System.out.println();;
+//		for(int i : ints) {
+//			System.out.printf("%s", (char) (i + (int)'A'));
+//		}
+//		System.out.println();;
+		
+		
+		int last = ints[chars.length-1];
+		for(int i = chars.length - 2; i >= 0; i--) {
+			if(last > ints[i]) {
+				
+				for(int k = chars.length-1; k > i; k--) {
+					ints[k] = ints[k-1];
+				}
+				ints[i] = last;
+				
+				nextPermutation(ints);
+				
+				break;
+			}
+		}
+		
+//		int[] array = {0, 1, 1, 1, 4};
+		
+		
+//		for(int i : ints) {
+//			System.out.printf("%d ", i);
+//		}
+//		System.out.println();
+		for(int i : ints) {
+			System.out.printf("%s", (char) (i + (int)'A'));
+		}
+	}
+	
+	static boolean nextPermutation(int[] array) {
+	    // Find longest non-increasing suffix
+	    int i = array.length - 1;
+	    while (i > 0 && array[i - 1] >= array[i])
+	        i--;
+	    // Now i is the head index of the suffix
+	    
+	    // Are we at the last permutation already?
+	    if (i <= 0)
+	        return false;
+	    
+	    // Let array[i - 1] be the pivot
+	    // Find rightmost element that exceeds the pivot
+	    int j = array.length - 1;
+	    while (array[j] <= array[i - 1])
+	        j--;
+	    // Now the value array[j] will become the new pivot
+	    // Assertion: j >= i
+	    
+	    // Swap the pivot with j
+	    int temp = array[i - 1];
+	    array[i - 1] = array[j];
+	    array[j] = temp;
+	    
+	    // Reverse the suffix
+	    j = array.length - 1;
+	    while (i < j) {
+	        temp = array[i];
+	        array[i] = array[j];
+	        array[j] = temp;
+	        i++;
+	        j--;
+	    }
+	    
+	    // Successfully computed the next permutation
+	    return true;
+	}
+
+}
---
+++ Jungol/src/sdk/level1/Problem1037.java
@@ -0,0 +1,92 @@
+package sdk.level1;
+
+import java.util.Scanner;
+
+public class Problem1037 {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[][] array = new int[n][n];
+		for(int i = 0; i < n; i++) {
+			for(int k = 0; k < n; k++) {
+				array[i][k] = in.nextInt();
+			}
+		}
+		in.close();
+		
+//		int n = 4;
+//		int[][] array = { 	{ 0, 0, 0, 0 },
+//				  			{ 0, 0, 0, 0 },
+//				  			{ 0, 0, 1, 0 },
+//				  			{ 0, 0, 0, 0 } };
+		
+		boolean isParitty = checkParitty(array);
+		
+		if(isParitty) {
+			System.out.println("OK");
+		} else {
+			changeParitty(array);
+		}
+	}
+	
+	public static void changeParitty(int[][] array) {
+		
+		int[] col = new int[array.length];
+		int[] row = new int[array.length];
+		
+		for(int i = 0; i < array.length; i++) {
+			for(int j = 0; j < array[i].length; j++) {
+				row[i] += array[i][j];
+				col[i] += array[j][i];
+			}
+		}
+		
+		int cols = getOddIndex(col); 
+		int rows = getOddIndex(row);
+		
+		if(cols != -1 && rows != -1) {
+			System.out.printf("Change bit (%d,%d)", rows+1, cols+1);
+		} else {
+			System.out.println("Corrupt");
+		}
+		
+	}
+	
+	public static int getOddIndex(int[] array) {
+		
+		int count = 0;
+		int index = 0;
+		for(int i = 0; i < array.length; i++) {
+			if(array[i] % 2 != 0) {
+				count++;
+				index = i;
+			}
+		}
+		
+		if(count >= 2) {
+			return -1;
+		}
+		
+		return index;
+	}
+	
+	public static boolean checkParitty(int[][] array) {
+		
+		int sumRow = 0; 
+		int sumCol = 0;
+		
+		for(int i = 0; i < array.length ; i++) {
+			for(int k = 0; k < array[i].length; k++) {
+				sumRow += array[i][k];
+				sumCol += array[k][i];
+			}
+			
+			if(sumRow%2 == 1 || sumCol%2 == 1)
+				return false;
+		}
+		
+		return true;
+	}
+}
