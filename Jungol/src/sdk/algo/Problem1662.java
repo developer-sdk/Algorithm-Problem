9ca316b826dc232ce16baf347c8771afaedf5cdd
118k <fluorite118@gmail.com>
2016-07-17 21:46:21 +0900

    2번째 알고리즘, 최대수 기준으로 처리, 50% 처

--- Jungol/src/sdk/algo/Problem1662.java
+++ Jungol/src/sdk/algo/Problem1662.java
@@ -6,100 +6,93 @@ import java.util.Scanner;
 public class Problem1662 {
 
 	static int maxValue = 0;
-	static int bishopN = 0;
 	
 	public static void main(String[] args) {
 		
+		int sumMax = 0;
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
 		int[][] array = new int[n][n];
 		for(int i = 0; i < n; i++) {
+			int temp = 0;
 			for(int j = 0; j < n; j++) {
 				array[i][j] = in.nextInt();
-				bishopN += array[i][j];
+				temp += array[i][j]; 
 			}
+			sumMax = (sumMax < temp) ? temp : sumMax;
 		}
 		in.close();
 		
 //		int n = 5;
-//		bishopN = 13;
 //		int[][] array = {	{ 1, 1, 0, 1, 1 }, 
 //							{ 0, 1, 0, 0, 0 }, 
 //							{ 1, 0, 1, 0, 1 }, 
 //							{ 1, 0, 0, 0, 0 }, 
 //							{ 1, 0, 1, 1, 1 }};
+//		sumMax = 4;
 		
-//		int n = 5;
-//		bishopN = 0;
-//		int[][] array = {	{ 0, 0, 0, 0, 0 }, 
-//							{ 0, 0, 0, 0, 0 }, 
-//							{ 0, 0, 0, 0, 0 }, 
-//							{ 0, 0, 0, 0, 0 }, 
-//							{ 0, 0, 0, 0, 0 }};
-		
-		
-		int[][] bishops = new int[bishopN][bishopN];
-		
-		int index = 0;
 		for(int x = 0; x < n; x++) {
-			for(int y = 0; y < n; y++ ) {
-				if(array[x][y] == 1)
-					bishops[index++] = new int[] { x, y };
+			int sum = 0;
+			for(int y = 0; y < n; y++) {
+				sum += array[x][y];
+			}
+			
+			if(sum == sumMax) {
+				
+				int result = check(array, x, sum);
+				maxValue = maxValue < result ? result : maxValue;
+//				System.out.println("------------");
+//				for(int[] xx : array){
+//					for(int yy : xx) {
+//						System.out.printf("%d ", yy);
+//					}
+//					System.out.println();
+//				}
+				
+				for(int i = 0; i < n; i++)
+					for(int j = 0; j <n ; j++)
+						if(array[i][j] == 9) array[i][j] = 1;
 			}
 		}
 		
-		int[] mate = new int[bishopN];
-		for(int i = 0; i < bishopN; i++) {
-			Arrays.fill(mate, 0);
-			mate[i] = 1;
-			check(bishops, mate, 1);
-			mate[i] = 0;
-		}
+		
 		
 		System.out.println(maxValue);
 	}
 	
-	public static void check(int[][] bishops, int[] mate, int count) {
+	public static int check(int[][] array, int pivotX, int count) {
 		
-		if(count >= bishopN) {
-			int sum = 0;
-			for(int m : mate) {
-				if(m == 1)
-					sum++;
-//				System.out.printf("%d ", m);
-			}
-				
-			maxValue = (maxValue < sum) ? sum : maxValue;
-//			System.out.println(sum);
-			return;
-		}
-		
-		for(int i = 0; i < mate.length; i++) {
+		for(int x = 0; x < array.length; x++) {
+			if(x == pivotX)
+				continue;
 			
-			if(mate[i] == 0) {
+			for(int y = 0; y < array.length; y++) {
 				
-				if(promising(bishops, mate, i)) {
-					mate[i] = 1;
-					check(bishops, mate, count+1);
-				} else {
-					mate[i] = -1;
-					check(bishops, mate, count+1);
+				if(array[x][y] == 1 && promising(array, pivotX, x, y)) {
+					array[x][y] = 9;
+					count++;
 				}
-				
-			} 
+					
+			}
 		}
+		
+		return count;
 	}
 
-	public static boolean promising(int[][] bishops, int[] mate, int index) {
-		int x = bishops[index][0];
-		int y = bishops[index][1];
+	public static boolean promising(int[][] array, int pivotX, int x, int y) {
 		
-		for(int i = 0; i < mate.length; i++) {
-			if(index != i && mate[i] == 1) {
-				int tX = bishops[i][0];
-				int tY = bishops[i][1];
-				
-				if(Math.abs(x-tX) == Math.abs(y-tY)) {
+		for(int pivotY = 0; pivotY < array.length; pivotY++) {
+			if(array[pivotX][pivotY] == 1 && Math.abs(x-pivotX) == Math.abs(y-pivotY)) {
+				return false;
+			}
+		}
+		
+		for(int i = 0; i < array.length; i++) {
+			if(i == pivotX)
+				continue;
+			
+			for(int j = 0; j < array.length; j++) {
+				if(array[i][j] == 9 && Math.abs(x-i) == Math.abs(y-j)) {
 					return false;
 				}
 			}
