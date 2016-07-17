321830b7d3b221e30df74939583bc2a634387f73
118k <fluorite118@gmail.com>
2016-07-17 22:52:43 +0900

    진행

--- Jungol/src/sdk/algo/Problem1662.java
+++ Jungol/src/sdk/algo/Problem1662.java
@@ -6,93 +6,111 @@ import java.util.Scanner;
 public class Problem1662 {
 
 	static int maxValue = 0;
+	static int bishopN = 0;
 	
 	public static void main(String[] args) {
 		
-		int sumMax = 0;
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
 		int[][] array = new int[n][n];
 		for(int i = 0; i < n; i++) {
-			int temp = 0;
 			for(int j = 0; j < n; j++) {
 				array[i][j] = in.nextInt();
-				temp += array[i][j]; 
+				bishopN += array[i][j];
 			}
-			sumMax = (sumMax < temp) ? temp : sumMax;
 		}
-		in.close();
+		in .close();
 		
 //		int n = 5;
+//		bishopN = 13;
 //		int[][] array = {	{ 1, 1, 0, 1, 1 }, 
 //							{ 0, 1, 0, 0, 0 }, 
 //							{ 1, 0, 1, 0, 1 }, 
 //							{ 1, 0, 0, 0, 0 }, 
 //							{ 1, 0, 1, 1, 1 }};
-//		sumMax = 4;
 		
+//		int n = 5;
+//		bishopN = 0;
+//		int[][] array = {	{ 0, 0, 0, 0, 0 }, 
+//							{ 0, 0, 0, 0, 0 }, 
+//							{ 0, 0, 0, 0, 0 }, 
+//							{ 0, 0, 0, 0, 0 }, 
+//							{ 0, 0, 0, 0, 0 }};
+		
+//		int n = 7;
+//		bishopN = 34;
+//		int[][] array = { 	{ 1, 0, 1, 1, 1, 0, 1 },
+//							{ 0, 1, 1, 1, 1, 1, 1 },
+//							{ 1, 1, 1, 1, 1, 1, 1 },
+//							{ 1, 0, 1, 1, 1, 1, 1 },
+//							{ 0, 1, 0, 0, 0, 1, 1 },
+//							{ 0, 1, 1, 1, 1, 1, 1 },
+//							{ 0, 0, 0, 0, 0, 0, 1 } };
+		
+		int[][] bishops = new int[bishopN][bishopN];
+		
+		int index = 0;
 		for(int x = 0; x < n; x++) {
-			int sum = 0;
-			for(int y = 0; y < n; y++) {
-				sum += array[x][y];
-			}
-			
-			if(sum == sumMax) {
-				
-				int result = check(array, x, sum);
-				maxValue = maxValue < result ? result : maxValue;
-//				System.out.println("------------");
-//				for(int[] xx : array){
-//					for(int yy : xx) {
-//						System.out.printf("%d ", yy);
-//					}
-//					System.out.println();
-//				}
-				
-				for(int i = 0; i < n; i++)
-					for(int j = 0; j <n ; j++)
-						if(array[i][j] == 9) array[i][j] = 1;
+			for(int y = 0; y < n; y++ ) {
+				if(array[x][y] == 1)
+					bishops[index++] = new int[] { x, y };
 			}
 		}
 		
-		
+		int[] mate = new int[bishopN];
+		for(int i = 0; i < bishopN; i++) {
+			Arrays.fill(mate, 0);
+			mate[i] = 1;
+			check(bishops, mate, 1, i);
+		}
 		
 		System.out.println(maxValue);
 	}
 	
-	public static int check(int[][] array, int pivotX, int count) {
+	public static void check(int[][] bishops, int[] mate, int count, int index) {
 		
-		for(int x = 0; x < array.length; x++) {
-			if(x == pivotX)
-				continue;
-			
-			for(int y = 0; y < array.length; y++) {
-				
-				if(array[x][y] == 1 && promising(array, pivotX, x, y)) {
-					array[x][y] = 9;
-					count++;
-				}
-					
+		if(count >= bishopN) {
+			int sum = 0;
+			for(int m : mate) {
+				if(m == 1)
+					sum++;
+				System.out.printf("%2d ", m);
 			}
+				
+			maxValue = (maxValue < sum) ? sum : maxValue;
+			System.out.println(sum);
+			return;
 		}
 		
-		return count;
+		for(int i = 0; i < index; i++) {
+			
+			if(mate[i] == 0) {
+				mate[i] = promising(bishops, mate, i) ? 1 : -1;
+				check(bishops, mate, count+1, i);
+			}
+		}
+
+		for(int i = index+1; i < mate.length; i++) {
+			
+			if(mate[i] == 0) {
+				mate[i] = promising(bishops, mate, i) ? 1 : -1;
+				check(bishops, mate, count+1, i);
+			}
+		} 
+		
+		
 	}
 
-	public static boolean promising(int[][] array, int pivotX, int x, int y) {
+	public static boolean promising(int[][] bishops, int[] mate, int index) {
+		int x = bishops[index][0];
+		int y = bishops[index][1];
 		
-		for(int pivotY = 0; pivotY < array.length; pivotY++) {
-			if(array[pivotX][pivotY] == 1 && Math.abs(x-pivotX) == Math.abs(y-pivotY)) {
-				return false;
-			}
-		}
-		
-		for(int i = 0; i < array.length; i++) {
-			if(i == pivotX)
-				continue;
-			
-			for(int j = 0; j < array.length; j++) {
-				if(array[i][j] == 9 && Math.abs(x-i) == Math.abs(y-j)) {
+		for(int i = 0; i < mate.length; i++) {
+			if(index != i && mate[i] == 1) {
+				int tX = bishops[i][0];
+				int tY = bishops[i][1];
+				
+				if(Math.abs(x-tX) == Math.abs(y-tY)) {
 					return false;
 				}
 			}
