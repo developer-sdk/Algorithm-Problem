479d2793b67312e8aa5888e695976f47b405731f
118k <fluorite118@gmail.com>
2016-07-22 23:17:24 +0900

    정올, 장기 1106 문

--- Jungol/src/sdk/algo/Problem1106.java
+++ Jungol/src/sdk/algo/Problem1106.java
@@ -21,35 +21,44 @@ public class Problem1106 {
 		int k = in.nextInt();
 		in.close();
 		
-//		int n = 9;
-//		int m = 9;
-//		int r = 3;
-//		int c = 5;
-//		int s = 2;
-//		int k = 8;
+//		int n = 65;
+//		int m = 75;
+//		int r = 50;
+//		int c = 11;
+//		int s = 64;
+//		int k = 56;
 		
 		xLength = n+1;
 		yLength = m+1;
-//		int[][] array = new int[n+1][m+1];
-//		array[r][c] = 1;
-//		array[s][k] = 2;
-		
+
 		Queue<int[]> queue = new LinkedList<int[]>();
 		queue.add(new int[] { r, c, 0 });
 		
 		bfs(queue, s, k);
 		
-//		for(int[] num : array) {
-//			for(int ns : num) {
-//				System.out.printf("%d ", ns);
-//			}
-//			System.out.println();
-//		}
-		
 		System.out.println(min);
 	}
 	
+	/**
+	 * 
+	 * 큐에 중복된 값이 있는지 확인 
+	 * 
+	 */
+	public static boolean queueCheck(Queue<int[]> queue, int x, int y) {
+		
+		for(int[] item : queue) {
+			if(item[0] == x && item[1] == y)
+				return false;
+		}
+		
+		return true;
+	}
 	
+	/**
+	 * x, y 값이 큐에입력될 수 있는 값인지 확인하고 입력한다. 
+	 * min 을 넘어서는지도 확인한다. 
+	 * 
+	 */
 	public static int check(int x, int y, int destX, int destY, int count,  Queue<int[]> queue) {
 		if( 0 < x && x < xLength && 0 < y && y < yLength) {
 			if(x == destX && y == destY) {
@@ -59,7 +68,8 @@ public class Problem1106 {
 				if(min < count+1)
 					return 1;
 				
-				queue.add(new int[] { x, y, count+1 });
+				if(queueCheck(queue, x, y))
+					queue.add(new int[] { x, y, count+1 });
 			}
 		}
 		
@@ -70,6 +80,7 @@ public class Problem1106 {
 		
 		int size = queue.size();
 		
+		// 장기말이 갈 수 있는 방향을 현재의 위치에 따라 구분하여 이동한다. 
 		while(size-- > 0) {
 			int[] location = queue.poll();
 			int x = location[0];
@@ -77,39 +88,48 @@ public class Problem1106 {
 			int count = location[2];
 			
 			if(destX <= x && destY <= y) {
-				// 좌상1
-				if(check(x-2, y-1, destX, destY, count, queue) == 1) 
-					return;
-				
-				if(check(x-1, y-2, destX, destY, count, queue) == 1)
-					return;
+				check(x-2, y-1, destX, destY, count, queue);
+				check(x-1, y-2, destX, destY, count, queue);
+			}
+			
+			if(destX < x && destY == y) {
+				check(x-1, y-2, destX, destY, count, queue);
+				check(x-1, y+2, destX, destY, count, queue);
 			}
 			
 			if(destX <= x && destY >= y) {
-				// 우
-				if(check(x-2, y+1, destX, destY, count, queue) == 1)
-					return;
-				
-				if(check(x-1, y+2, destX, destY, count, queue) == 1)
-					return;
+				check(x-2, y+1, destX, destY, count, queue);
+				check(x-1, y+2, destX, destY, count, queue);
+			}
+			
+			if(destX == x && destY > y) {
+				check(x-2, y+1, destX, destY, count, queue);
+				check(x+2, y+1, destX, destY, count, queue);
 			}
 			
 			if(destX >= x && destY >= y) {
-				if(check(x+2, y+1, destX, destY, count, queue) == 1)
-					return;
-				
-				if(check(x+1, y+2, destX, destY, count, queue) == 1)
-					return;
+				check(x+2, y+1, destX, destY, count, queue);
+				check(x+1, y+2, destX, destY, count, queue);
+			}
+			
+			if(destX > x && destY == y) {
+				check(x+1, y-2, destX, destY, count, queue);
+				check(x+1, y+2, destX, destY, count, queue);
 			}
 			
 			if(destX >= x && destY <= y) {
-				if(check(x+2, y-1, destX, destY, count, queue) == 1)
-					return;
-				
-				if(check(x+1, y-2, destX, destY, count, queue) == 1)
-					return;
+				check(x+2, y-1, destX, destY, count, queue);
+				check(x+1, y-2, destX, destY, count, queue);
+			}
+			
+			if(destX == x && destY < y) {
+				check(x-2, y-1, destX, destY, count, queue);
+				check(x+2, y-1, destX, destY, count, queue);
 			}
 		}
+		
+		if(min != Integer.MAX_VALUE)
+			return;
 		
 		bfs(queue, destX, destY);
 	}
