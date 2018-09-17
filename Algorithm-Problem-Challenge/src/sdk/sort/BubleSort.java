5eada3ae66fb42f7ae451e7faa7068a5869ef8f0
118k <fluorite118@gmail.com>
2018-09-17 22:15:49 +0900

    버블소트 수정

---
+++ Algorithm-Problem-Challenge/src/sdk/sort/BubleSort.java
@@ -0,0 +1,32 @@
+package sdk.sort;
+
+import java.util.Arrays;
+
+public class BubleSort {
+
+    static int[] array = { 9, 3, 5, 4, 6, 1, 8, 2 };
+
+    public static void sort(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < array.length - i; j++) {
+
+                // 현재원소와 바로 앞 원소를 비교하여 큰쪽을 뒤로 넘긴다.
+                if (array[j - 1] > array[j])
+                    swap(j - 1, j);
+            }
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void swap(int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        BubleSort.sort(array);
+    }
+}
