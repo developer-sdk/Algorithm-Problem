937b6ab213e4d95455f5068c9c5fb4c861ce48bd
118k <fluorite118@gmail.com>
2019-01-09 23:14:56 +0900

    백준 1600

--- Algorithm-Problem-Challenge/src/sdk/backjun/bfs/Problem1600.java
+++ Algorithm-Problem-Challenge/src/sdk/backjun/bfs/Problem1600.java
@@ -1,5 +1,124 @@
 package sdk.backjun.bfs;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * 말이 되고픈 원숭이
+ * 
+ * @author User
+ *
+ */
 public class Problem1600 {
 
+	static int[][] moveMonkey = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+	static int[][] moveHorse = { { -2, -1 }, { -1, -2 }, { 1, -2 }, { 2, -1 }, { 2, 1 }, { 1, 2 }, { -1, 2 },
+			{ -2, 1 } };
+
+	static int K; // 말이 되어 움직인 횟수
+	static int W; // 가로, y
+	static int H; // 세로, x
+	static int[][] map;
+	static int[][][] min;
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		K = sc.nextInt();
+		W = sc.nextInt();
+		H = sc.nextInt();
+
+		map = new int[H][W];
+		min = new int[K + 1][H][W];
+
+		for (int x = 0; x < H; x++) {
+			for (int y = 0; y < W; y++) {
+				map[x][y] = sc.nextInt();
+
+				for (int k = 0; k < K + 1; k++) {
+					min[k][x][y] = Integer.MAX_VALUE;
+				}
+			}
+		}
+
+		sc.close();
+
+		Monkey start = new Monkey(0, 0, 0, 0);
+		int result = move(start);
+
+		System.out.println(result);
+	}
+
+	public static int move(Monkey start) {
+
+		Queue<Monkey> queue = new LinkedList<>();
+		queue.add(start);
+
+		while (!queue.isEmpty()) {
+
+			Monkey current = queue.poll();
+
+			moveMonkey(current, queue);
+
+			if (current.k < K)
+				moveHorse(current, queue);
+		}
+
+		return min();
+	}
+
+	private static int min() {
+
+		int result = Integer.MAX_VALUE;
+		for (int k = 0; k < K + 1; k++) {
+			result = Math.min(result, min[k][H - 1][W - 1]);
+		}
+
+		return result == Integer.MAX_VALUE ? -1 : result;
+	}
+
+	private static void moveHorse(Monkey current, Queue<Monkey> queue) {
+		move(current, moveHorse, current.k + 1, queue);
+	}
+
+	private static void moveMonkey(Monkey current, Queue<Monkey> queue) {
+		move(current, moveMonkey, current.k, queue);
+	}
+
+	private static void move(Monkey current, int[][] moveArr, int nextK, Queue<Monkey> queue) {
+		for (int[] move : moveArr) {
+			int nextX = current.x + move[0];
+			int nextY = current.y + move[1];
+			int nextCount = current.c + 1;
+
+			if (isMoveable(nextX, nextY, nextK, nextCount)) {
+				queue.add(new Monkey(nextX, nextY, nextK, nextCount));
+				min[nextK][nextX][nextY] = nextCount;
+			}
+		}
+	}
+
+	private static boolean isMoveable(int x, int y, int k, int c) {
+
+		if (0 <= x && x < H && 0 <= y && y < W && map[x][y] != 1 && min[k][x][y] > c)
+			return true;
+
+		return false;
+	}
+}
+
+class Monkey {
+	public int x;
+	public int y;
+	public int k;
+	public int c;
+
+	public Monkey(int x, int y, int k, int c) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.k = k;
+		this.c = c;
+	}
 }
