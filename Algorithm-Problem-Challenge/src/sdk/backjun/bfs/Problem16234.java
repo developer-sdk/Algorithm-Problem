de17a95f647a01211c1b13aab25c417f1325553f
118k <fluorite118@gmail.com>
2019-02-08 23:24:38 +0900

    백준, 16234, 인구이동

--- Algorithm-Problem-Challenge/src/sdk/backjun/bfs/Problem16234.java
+++ Algorithm-Problem-Challenge/src/sdk/backjun/bfs/Problem16234.java
@@ -1,7 +1,7 @@
 package sdk.backjun.bfs;
 
 import java.util.LinkedList;
-import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 
 /**
@@ -13,105 +13,121 @@ import java.util.Scanner;
  */
 public class Problem16234 {
 
-	static int n;
-	static int L;
-	static int R;
-	static int map[][];
-	static boolean visited[][];
+	public static int[][] moves = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+	public static int N; // 땅의 크기
+	public static int L; // L명 이상
+	public static int R; // R명 이하
 
-	static int dx[] = { 1, -1, 0, 0 };
-	static int dy[] = { 0, 0, 1, -1 };
+	public static int[][] map;
+	public static boolean[][] visited;
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
+		N = sc.nextInt();
 		L = sc.nextInt();
 		R = sc.nextInt();
-		int sec = 0;
-		map = new int[n][n];
 
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				map[i][j] = sc.nextInt();
-			}
-		}
+		map = new int[N][N];
+		
+
+		for (int x = 0; x < N; x++)
+			for (int y = 0; y < N; y++)
+				map[x][y] = sc.nextInt();
 
 		sc.close();
 
+		int count = 0;
 		while (true) {
-			visited = new boolean[n][n]; // 방문 초기화
-			if (!check()) {
-				sec++; // 인구이동이 또 필요한 경우
+
+			boolean hasNext = false;
+			visited = new boolean[N][N];
+
+			for (int x = 0; x < N; x++) {
+				for (int y = 0; y < N; y++) {
+
+					// 방문여부, 연합의 개수를 확인하여 이번 턴에 연합이 있는지 확인 
+					if (!visited[x][y] && check(x, y) > 1) {
+						hasNext = true;
+					}
+				}
+			}
+
+			if (hasNext) {
+				count++;
 			} else {
 				break;
 			}
 		}
 
-		System.out.println(sec);
-
+		System.out.println(count);
 	}
 
-	// 인구 이동 필요한지 전체 지도 확인.
-	public static boolean check() {
-		List<Node> n_list;
-		boolean isDone = true; // 이동이 더이상 필요하지 않을 경우 true. 필요한 경우 false
+	// 연합 확인 
+	public static int check(int startX, int startY) {
 
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				// 방문하지 않은 경우
-				if (!visited[i][j]) {
-					n_list = new LinkedList<>();
-					n_list.add(new Node(i, j));
-					// sum - 리스트에 저장된 값의 합.
-					int sum = dfs(i, j, n_list, 0);
-					if (n_list.size() > 1) { // 리스트 크기가 1 이상일 경우(= 인구이동이 필요한 데이터가 있을경우)
-						change(sum, n_list); // 평균값 계산해서 map 갱신.
-						isDone = false;
-					}
+		LinkedList<Nation> allies = new LinkedList<>();
+		Queue<Nation> queue = new LinkedList<>();
+		Nation start = new Nation(startX, startY);
+		queue.add(start);
+		allies.add(start);
+		visited[startX][startY] = true;
+		int sum = map[startX][startY];
+		
+		while (!queue.isEmpty()) {
+
+			Nation current = queue.poll();
+
+			for (int[] move : moves) {
+				int nextX = current.x + move[0];
+				int nextY = current.y + move[1];
+
+				if (isMoveable(current, nextX, nextY)) {
+					Nation next = new Nation(nextX, nextY);
+					queue.add(next);
+					allies.add(next);
+					sum += map[nextX][nextY];
+					visited[nextX][nextY] = true;
 				}
 			}
 		}
-		return isDone;
-	}
 
-	// 평균값으로 map 갱신.
-	public static void change(int sum, List<Node> n_list) {
-		int avg = sum / n_list.size();
-		for (Node node : n_list) {
-			map[node.x][node.y] = avg;
-		}
-	}
+		// 연합이 있으면 연합의 인구수 변경 
+		if (allies.size() != 1) {
+			int result = sum / allies.size();
 
-	public static int dfs(int x, int y, List<Node> n_list, int sum) {
-		visited[x][y] = true;
-		sum = map[x][y];
-
-		for (int i = 0; i < 4; i++) {
-			int next_x = x + dx[i];
-			int next_y = y + dy[i];
-
-			if (next_x < 0 || next_x >= n || next_y < 0 || next_y >= n) {
-				continue;
-			}
-			if (!visited[next_x][next_y]) {
-				int d = Math.abs(map[x][y] - map[next_x][next_y]);
-				if (d >= L && d <= R) {
-					n_list.add(new Node(next_x, next_y));
-					sum += dfs(next_x, next_y, n_list, sum);
-				}
+			for (Nation ally : allies) {
+				map[ally.x][ally.y] = result;
 			}
 		}
-		return sum;
+
+		// 연합이 없으면 1반환 
+		return allies.size();
 	}
 
-}
+	// 이동 가능 여부 확인 
+	private static boolean isMoveable(Nation current, int nextX, int nextY) {
 
-class Node {
-	int x;
-	int y;
+		if (0 <= nextX && nextX < N && 0 <= nextY && nextY < N && !visited[nextX][nextY]) {
 
-	public Node(int x, int y) {
-		this.x = x;
-		this.y = y;
+			// 인구수의 차이 확인 
+			int abs = Math.abs(map[current.x][current.y] - map[nextX][nextY]);
+
+			if (L <= abs && abs <= R)
+				return true;
+		}
+
+		return false;
+	}
+
+	static class Nation {
+		public int x;
+		public int y;
+
+		public Nation(int x, int y) {
+			super();
+			this.x = x;
+			this.y = y;
+		}
 	}
 }
